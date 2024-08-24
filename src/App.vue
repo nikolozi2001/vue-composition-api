af6f164c05180b816d1175b741a7195bb82c35ad
nikolozi2001 <mr.qachibaia44@gmail.com>
2024-08-24 13:27:38 +0400

    Creating reactive object with the reactive function

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 export default {
   setup() {
     const message = ref("Hello");
     const quantity = ref(1);
+    const item = reactive({
+      name: "Product 1",
+      price: 10.99,
+    });
 
-    const increment = () => quantity.value++
-    const decrement = () => quantity.value--
+    const increment = () => quantity.value++;
+    const decrement = () => quantity.value--;
+
+    const swapProduct = () => {
+      item.name = "Product A";
+      item.price = 9.99;
+    };
 
     return {
       message,
       quantity,
       increment,
-      decrement
+      decrement,
+      item,
+      swapProduct,
     };
   },
 };
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
+  <h1>{{ item.name }} : {{ item.price }}</h1>
+  <button @click="swapProduct">Swap Product</button>
   <p>Quantity: {{ quantity }}</p>
   <button @click="increment">+</button>
   <button @click="decrement">-</button>

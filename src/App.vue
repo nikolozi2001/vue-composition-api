bec77190a1a554f8f7333aecbbfc58af030b1030
nikolozi2001 <mr.qachibaia44@gmail.com>
2024-08-24 13:18:56 +0400

    Creating reactive reference with the ref function

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const message = ref("Hello");
+    const quantity = ref(1);
+
+    const increment = () => quantity.value++
+    const decrement = () => quantity.value--
+
+    return {
+      message,
+      quantity,
+      increment,
+      decrement
+    };
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ message }}</h1>
+  <p>Quantity: {{ quantity }}</p>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+  <input type="text" v-model="message" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>

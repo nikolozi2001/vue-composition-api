81a8cd4ec073e16dfde67a851de3f3ab2f571cf3
nikolozi2001 <mr.qachibaia44@gmail.com>
2024-08-24 14:04:55 +0400

    Computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,34 @@
 <script>
-import { ref, reactive, toRef, toRefs } from "vue";
+import { computed, ref, reactive, toRef, toRefs } from "vue";
 
 export default {
   setup() {
-    const message = ref("Hello");
-    const quantity = ref(1);
     const item = reactive({
       name: "Product 1",
       price: 10.99,
+      quantity: 1,
     });
 
-    const increment = () => quantity.value++;
-    const decrement = () => quantity.value--;
+    const increment = () => item.quantity++;
+    const decrement = () => item.quantity--;
 
     const swapProduct = () => {
       item.name = "Product A";
       item.price = 9.99;
     };
 
-    const { name, price } = toRefs(item);
+    const total = computed(() => item.price * item.quantity);
+
+    const { name, price, quantity } = toRefs(item);
 
     return {
-      message,
       quantity,
       increment,
       decrement,
       item,
       name,
       price,
+      total,
       swapProduct,
     };
   },
@@ -37,10 +38,11 @@ export default {
 <template>
   <h1>{{ name }} : {{ price }}</h1>
   <button @click="swapProduct">Swap Product</button>
+  <button @click="price++">Increment Price</button>
   <p>Quantity: {{ quantity }}</p>
   <button @click="increment">+</button>
   <button @click="decrement">-</button>
-  <input type="text" v-model="message" />
+  <p>Total: {{ total }}</p>
 </template>
 
 <style scoped></style>
